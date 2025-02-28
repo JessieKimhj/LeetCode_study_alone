fa90aad6298fe61328ea8dec8aa054b2068e4ec3
JessieKimhj <dpro5893@gmail.com>
2025-02-27 23:43:48 -0800

    Remove Element Solution

---
+++ Remove Element.go
@@ -0,0 +1,12 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	writeIndex := 0
+	for readIndex := 0; readIndex < len(nums); readIndex++ {
+		if nums[readIndex] != val {
+			nums[writeIndex] = nums[readIndex]
+			writeIndex++
+		}
+	}
+	return writeIndex
+}
