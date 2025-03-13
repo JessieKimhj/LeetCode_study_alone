ff998e8ba8e39517be9c282cd1993992cfa3be7e
JessieKimhj <dpro5893@gmail.com>
2025-03-13 01:02:07 -0700

    add description Sqrt(x) Solution

--- Sqrt(x).go
+++ Sqrt(x).go
@@ -22,6 +22,15 @@ function mySqrt(x: number): number {
   // as per the constrain x lies from 0 - 2^31-1
   let low = 0;
 
+  // to calculate square root of number means
+  // we need to find the square of that function
+  // meaning if 4 is the target then 2 will be it's
+  // input as 2 x 2 == 4
+  // meaning the square will always be smaller than
+  // the squareroot i.e 4 > 2
+  // so high pointer will be the number itself as
+  // the higher we go the further away we go from
+  // the square of the that target
   let high = x;
 
   while (low <= high) {
